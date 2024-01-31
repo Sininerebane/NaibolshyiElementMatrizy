10893bc80f98a5d1f4407f14caa87fc3e60fd012
CORP-HR\natalya.berezovski <natalya.berezovski@hireright.com>
2024-01-31 23:25:57 +0200

    Add project files.

---
+++ NaibolshyiElementMatrizy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NaibolshyiElementMatrizy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int size = 10;
+            int maxItem = int.MinValue;
+            int[,] matrix = new int[size, size];
+            int minElement = 1;
+            int maxElement = 20;
+            int replacementMaxNumber = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(minElement, maxElement);
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] >= maxItem)
+                    {
+                        maxItem = matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Максимальный элемент массива равен " + maxItem);
+            Console.WriteLine();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == maxItem)
+                    {
+                        matrix[i, j] = replacementMaxNumber;
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
